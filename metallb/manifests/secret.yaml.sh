364dfa18cdcfcaf867dea0523ec1f3fd80db0ba5
asauerwein <andreas@sauerwein.se>
2021-06-06 23:26:24 +0200

    change secret name and add namespace

--- metallb/manifests/secret.yaml.sh
+++ metallb/manifests/secret.yaml.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # just generate random 256 character alphanumeric string and create a sealed secret
+namespace='metallb'
 random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-256} | head -n 1)
-echo -n $random_string | kubectl create secret generic mysecret --dry-run=client --from-file=speaker-secret=/dev/stdin -o json | kubeseal > sealedsecret.yaml -o yaml
+echo -n $random_string | kubectl create secret generic metallb-speaker-secret -n $namespace --dry-run=client --from-file=speaker-secret=/dev/stdin -o json | kubeseal > sealedsecret.yaml -o yaml
